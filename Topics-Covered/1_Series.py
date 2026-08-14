740f127b9ea90729d735ec422189d3f11b6e85f4
Khushnood Ali Khan <khushnoodali287@gmail.com>
2026-08-15 01:06:23 +0500

    series

--- Topics-Covered/1_Series.py
+++ Topics-Covered/1_Series.py
@@ -5,14 +5,24 @@ import pandas as pd
 #  Series: One Dimensional array holding data of any type.
 #           Like a column in a table
 
-data = ["Kareem", "Nabeel","Shareef","Jameel"]
+# data = ["Kareem", "Nabeel","Shareef","Jameel"]
 
 # series = pd.Series(data)        #converting python list into series (or a column), but without a label or index
 # print(series)
 
-series = pd.Series(data, index=['Web Dev', 'Data Analy','Cyber Sec','Ai Eng'])    # PASSING LABELS TO DATA
+# PASSING LABELS TO DATA
+# series = pd.Series(data, index=['Web Dev', 'Data Analy','Cyber Sec','Ai Eng'])
 # print(series.loc['Ai Eng'])         # ACCESSING THE DATA WITH LABEL
 
 # CHANGING THE VALUE
-series.loc['Ai Eng'] = "Sameer"
-print(series)
+# series.loc['Ai Eng'] = "Sameer"
+# print(series)
+
+# ACCESSING THROUGH INDEX NUMBER
+# print(series.iloc[2])
+
+
+# RETURN/ACCESS/FILTER BY VALUE
+data_list = [100, 104, 108, 112, 116, 120]
+list_to_series = pd.Series(data_list, index=['a','b','c','d','e','f'])
+print(list_to_series[list_to_series > 110])
