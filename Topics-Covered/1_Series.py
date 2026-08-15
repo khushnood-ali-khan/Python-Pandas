a970c742bc589e48e87430106b8a712c9f921e9a
Khushnood Ali Khan <khushnoodali287@gmail.com>
2026-08-15 19:50:49 +0500

    Series

--- Topics-Covered/1_Series.py
+++ Topics-Covered/1_Series.py
@@ -1,25 +1,25 @@
 import pandas as pd
 
-# print(pd.__version__)       # check pandas version
+print(pd.__version__)       # check pandas version
 
-#  Series: One Dimensional array holding data of any type.
-#           Like a column in a table
+""" Series: One Dimensional array holding data of any type.
+            Like a column in a table
+"""
+data = ["Kareem", "Nabeel","Shareef","Jameel"]
 
-# data = ["Kareem", "Nabeel","Shareef","Jameel"]
-
-# series = pd.Series(data)        #converting python list into series (or a column), but without a label or index
-# print(series)
+series = pd.Series(data)        #converting python list into series (or a column), but without a label or index
+print(series)
 
 # PASSING LABELS TO DATA
-# series = pd.Series(data, index=['Web Dev', 'Data Analy','Cyber Sec','Ai Eng'])
-# print(series.loc['Ai Eng'])         # ACCESSING THE DATA WITH LABEL
+series = pd.Series(data, index=['Web Dev', 'Data Analy','Cyber Sec','Ai Eng'])
+print(series.loc['Ai Eng'])         # ACCESSING THE DATA WITH LABEL
 
 # CHANGING THE VALUE
-# series.loc['Ai Eng'] = "Sameer"
-# print(series)
+series.loc['Ai Eng'] = "Sameer"
+print(series)
 
 # ACCESSING THROUGH INDEX NUMBER
-# print(series.iloc[2])
+print(series.iloc[2])
 
 
 # RETURN/ACCESS/FILTER BY VALUE
